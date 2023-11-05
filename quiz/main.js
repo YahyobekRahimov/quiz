14209d387f1a1e372413371b1197889b940341b5
YahyobekRahimov <rcoder570@gmail.com>
2023-11-06 02:27:27 +0500

    Added some features like storing information about user response in the storage

--- quiz/main.js
+++ quiz/main.js
@@ -1,8 +1,9 @@
+// ! DOM elements
 const QUESTION_OUTPUT = document.getElementById('question-output');
-const OPTION_A = document.getElementById('option-a');
-const OPTION_B = document.getElementById('option-b');
-const OPTION_C = document.getElementById('option-c');
-const OPTION_D = document.getElementById('option-d');
+const OPTION_A = document.getElementById('optionA');
+const OPTION_B = document.getElementById('optionB');
+const OPTION_C = document.getElementById('optionC');
+const OPTION_D = document.getElementById('optionD');
 
 const QUESTION_LINE_1 = document.getElementById('question-line-1');
 const QUESTION_LINE_2 = document.querySelectorAll('.question-line-2');
@@ -22,6 +23,10 @@ const NEXT_BUTTON = document.getElementById('next-button');
 
 const SCORE_SECTION = document.getElementById('score-section');
 const SCORE_OUTPUT = document.getElementById('score-output');
+
+const RADIO_BUTTONS = document.querySelectorAll('input[type="radio"]');
+
+// ! Resources and variables
 
 const QUESTIONS = [
     {
@@ -70,10 +75,150 @@ const QUESTIONS = [
         answer: "C"
     }
 ];
+
+let questionIndex = 0;
+let score = 0;
+
+let result = JSON.parse(localStorage.getItem('result')) || [];
+
+// ! Initial function calls
+
+disableOrEnablePreviousButton(questionIndex);
+
 document.addEventListener('DOMContentLoaded', function() {
-    QUESTION_OUTPUT.innerHTML = `${QUESTIONS[0].id}. ${QUESTIONS[0].question}`;
-    OPTION_A.innerHTML = QUESTIONS.optionA;
-    OPTION_B.innerHTML = QUESTIONS.optionB;
-    OPTION_C.innerHTML = QUESTIONS.optionC;
-    OPTION_D.innerHTML = QUESTIONS.optionD;
+    addQuestion(questionIndex);
+});
+
+// ! Listeners
+
+NEXT_BUTTON.addEventListener('click', function() {
+    increaseScore(questionIndex, saveToLocalStorage);
+    questionIndex++;
+    removeChecked();
+    addQuestion(questionIndex, handleCheckedResponse);
+    if (questionIndex === 1) {
+        disableOrEnablePreviousButton(questionIndex);
+    }
+    if (questionIndex === 4) {
+        addSubmitButton();
+    };
 })
+
+
+
+PREVIOUS_BUTTON.addEventListener('click', function() {
+    questionIndex--;
+    addQuestion(questionIndex, handleCheckedResponse);
+    if (questionIndex === 3) {
+        removeSubmitButton();
+    }
+    if (questionIndex === 0) {
+        disableOrEnablePreviousButton(questionIndex);
+    }
+})
+
+// ! Functions
+
+function removeChecked() {
+    RADIO_BUTTONS.forEach(element => {
+        element.checked = false;
+    })
+}
+
+function addSubmitButton() {
+    NEXT_BUTTON.classList.add('submit-button');
+    NEXT_BUTTON.textContent = 'Submit';
+}
+function increaseScore(questionIndex, saveToLocalStorage) {
+    RADIO_BUTTONS.forEach((element, optionIndex) => {
+        let option;
+        let isCorrect = false;
+        if (!element.checked) {
+            return;
+        }
+        switch (optionIndex) {
+            case 0:
+                option = 'A';
+                break;
+            case 1:
+                option = 'B';
+                break;
+            case 2:
+                option = 'C';
+                break;
+                case 3:
+                    option = 'D';
+                break;
+                default:
+                    break;
+        }
+        if (option === QUESTIONS[questionIndex].answer) {
+            score++;
+            isCorrect = true;
+        };
+        saveToLocalStorage(isCorrect, optionIndex, questionIndex);
+    });
+}
+
+function saveToLocalStorage(isCorrect, optionIndex, questionIndex) {
+    if (result[questionIndex]) {
+        result[questionIndex].isCorrect = isCorrect;
+        result[questionIndex].optionIndex = optionIndex;
+        console.log('Updated your response');
+        localStorage.setItem('result', JSON.stringify(result));
+        return;
+    }
+    let response = {};
+    response.isCorrect = isCorrect;
+    response.optionIndex = optionIndex;
+    result.push(response);
+    console.log('Added new response');
+    localStorage.setItem('result', JSON.stringify(result));
+}   
+
+function removeSubmitButton() {
+    NEXT_BUTTON.classList.remove('submit-button');
+    NEXT_BUTTON.textContent = 'Next';
+}
+
+function disableOrEnableNextButton(questionIndex) {
+    if (questionIndex === 4) {
+        NEXT_BUTTON.setAttribute('disabled', '');
+        NEXT_BUTTON.style.opacity = '0.4';
+    } else {
+        NEXT_BUTTON.removeAttribute('disabled');
+        NEXT_BUTTON.style.opacity = '1';
+    }
+}
+function addQuestion(questionIndex, handleCheckedResponse) {
+    QUESTION_OUTPUT.innerHTML = `${QUESTIONS[questionIndex].id}. ${QUESTIONS[questionIndex].question}`;
+    OPTION_A.innerHTML = QUESTIONS[questionIndex].optionA;
+    OPTION_B.innerHTML = QUESTIONS[questionIndex].optionB;
+    OPTION_C.innerHTML = QUESTIONS[questionIndex].optionC;
+    OPTION_D.innerHTML = QUESTIONS[questionIndex].optionD;
+    handleCheckedResponse(questionIndex);
+}
+
+function handleCheckedResponse(questionIndex) {
+    let result = JSON.parse(localStorage.getItem('result')) || [];
+    let optionIndex;
+    if (result[questionIndex]) {
+        optionIndex = result[questionIndex].optionIndex;
+        RADIO_BUTTONS.forEach((element, index) => {
+            if (optionIndex == index) {
+                element.checked = true;
+            }
+        })
+    }
+}
+
+function disableOrEnablePreviousButton(questionIndex) {
+    if (questionIndex === 0) {
+        PREVIOUS_BUTTON.setAttribute('disabled', '');
+        PREVIOUS_BUTTON.style.opacity = '0.4';
+    } else {
+        PREVIOUS_BUTTON.removeAttribute('disabled');
+        PREVIOUS_BUTTON.style.opacity = '1';
+    }
+}
+
